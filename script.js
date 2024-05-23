75e34684d157ab13fd921c3a09b333247d2e1636
Neyna Nayak <140084295+neyhere07@users.noreply.github.com>
2024-05-23 14:04:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+  const openModalButton = document.getElementById("openModal");
+  const modal = document.getElementById("modal");
+  const closeModalButton = document.querySelector(".close-modal");
+
+  openModalButton.addEventListener("click", function () {
+    console.log("click");
+    modal.style.display = "block";
+  });
+
+  closeModalButton.addEventListener("click", function () {
+    modal.style.display = "none";
+  });
+
+  window.addEventListener("click", function (event) {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+});
